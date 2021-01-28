aa050ec0d7fb0a08f2b98fa50f6514840cb2d093
Bodale Marin <74355214+mbodale@users.noreply.github.com>
2021-01-28 10:43:03 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 sudo apt-get update
 sudo apt-get upgrade
 sudo apt-get install git-core ruby cnetworkmanager
-# ssh-keygen -t rsa -C pauly@clarkeology.com
+# ssh-keygen -t rsa -C mbodale@yahoo.com
 vi ~/.ssh/id_rsa.pub # and paste into https://github.com/settings/ssh
-git clone git://github.com/pauly/lightwaverf.git # or git clone git@github.com:pauly/lightwaverf.git
+git clone git://github.com/mbodale/lightwaverf.git # or git clone git@github.com:mbodale/lightwaverf.git
 cd lightwaverf/
 sudo gem install lightwaverf
 # gem build lightwaverf.gemspec && sudo gem install ./lightwaverf-0.2.1.gem # or whatever the latest version is
